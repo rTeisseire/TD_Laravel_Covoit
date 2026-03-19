e698c46a645c8aaa67ede3f390f728f8b83b29f3
rTeisseire <teisseireraphael@gmail.com>
2026-03-19 13:46:49 +0100

    Ajout des alias pour les middlewares EmployeAUnCampus, EmployeAUneVoiture et NombrePlacesVoiture

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\EmployeAUnCampus;
+use App\Http\Middleware\EmployeAUneVoiture;
+use App\Http\Middleware\NombrePlacesVoiture;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'employe.campus' => EmployeAUnCampus::class,
+            'employe.voiture' => EmployeAUneVoiture::class,
+            'voiture.places' => NombrePlacesVoiture::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
