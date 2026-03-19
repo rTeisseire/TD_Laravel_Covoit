ea41e44f6d7e27740b08cc81b2f339d007c5d816
rTeisseire <teisseireraphael@gmail.com>
2026-03-19 13:49:42 +0100

    Ajout des routes pour la gestion des employés et des voitures

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@ Route::get('/voitures/filtrer-places', [VoitureController::class, 'filtrerParNom
 Route::get('/voitures/filtrer-modele', [VoitureController::class, 'filtrerParModele'])->name('voitures.filtrerModele');
 
 Route::resource('voitures', VoitureController::class);
+
+Route::get('/employes', [EmployeController::class, 'index'])->name('employes.index');
+
+Route::get('/employes/{employe}', [EmployeController::class, 'show'])
+    ->middleware(['employe.campus', 'employe.voiture'])
+    ->name('employes.show');
+
+Route::get('/employes/{employe}/ajouter-voiture', [VoitureController::class, 'createForEmploye'])
+    ->name('employes.addVoitureForm');
+Route::post('/employes/{employe}/ajouter-voiture', [VoitureController::class, 'storeForEmploye'])
+    ->name('employes.storeVoiture');
+
+Route::get('/voitures/{voiture}', [VoitureController::class, 'show'])
+    ->middleware('voiture.places')
+    ->name('voitures.show');
