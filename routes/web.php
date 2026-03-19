73aeb9f6cd4256595e8895ce3647bcb862096d27
rTeisseire <teisseireraphael@gmail.com>
2026-03-19 08:31:01 +0100

    Ajout de la dépendance barryvdh/laravel-debugbar et mise à jour des fichiers de configuration. Ajout de la ressource vehicules dans les routes.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeController;
+use App\Http\Controllers\VehiculeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('employes', EmployeController::class);
+
+Route::get('/vehicules/filtrer-places', [VehiculeController::class, 'filtrerParNombreDePlaces']);
+Route::get('/vehicules/filtrer-modele', [VehiculeController::class, 'filtrerParModele']);
+
+Route::resource('vehicules', VehiculeController::class);
